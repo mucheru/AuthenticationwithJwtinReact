ec38e4d82788268b593005e84051c93e5d322422
steve <stephen@stephenmucheru79@gmail.com>
2023-08-30 14:37:14 +0300

    files of authentication using react framework

---
+++ src/components/BoardAdmin.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+import UserService from "../services/user.service";
+
+const BoardAdmin = () => {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    UserService.getAdminBoard().then(
+      (response) => {
+        setContent(response.data);
+      },
+      (error) => {
+        const _content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+
+        setContent(_content);
+      }
+    );
+  }, []);
+
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h3>{content}</h3>
+      </header>
+    </div>
+  );
+};
+
+export default BoardAdmin;
---
+++ src/services/auth.service.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const  API_URL = "http://localhost:8080/api/auth/";
+
+const register = (username,email,password)=>{
+    return axios.post(API_URL+"signup",{
+        username,
+        email,
+        password
+    });
+};
+
+const login = async (username,password)=>{
+    const response = await axios.post(API_URL + "signin", {
+        username,
+        password
+    });
+    if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+    }
+    return response.data;
+};
+
+const logout = ()=>{
+    localStorage.removeItem("user")
+}
+
+const getCurrentUser = ()=>{
+    return JSON.parse(localStorage.getItem("user"));
+}
+
+const AuthService = {
+    login,register,logout,getCurrentUser
+}
+
+export default AuthService;
